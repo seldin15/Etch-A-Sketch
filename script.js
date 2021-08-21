68a77d4f113105fac28eec6cc3713e32e3644df9
seldin15 <seldin_r@hotmail.com>
2021-08-22 00:07:25 +0200

    Added a button click event, needs tweaking of H+W of columns and rows to work

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-function drawGrid() {
-    for(let i = 0; i < 16; i++) {
+window.onload(drawGrid(32));
+const button = document.getElementById('play');
+
+button.addEventListener("click", () => {
+    resetGrid();
+});
+
+function drawGrid(gridSize) {
+    for(let i = 0; i < gridSize; i++) {
         const rows = document.createElement('div');
         rows.classList.add('rows');
         document.querySelector(".containerGrid").appendChild(rows);
-        for(let j = 0; j < 16; j++) {
+        for(let j = 0; j < gridSize; j++) {
             const columns = document.createElement('div');
             columns.classList.add('columns');
             rows.appendChild(columns);
+            columns.addEventListener("mouseenter", function(event) {
+                event.target.style.backgroundColor = "yellow";
+            });
         }
 }
+}
+
+function resetGrid() {
+    let gridSize = prompt("Type in grid size (Max 100)", "");
+    removeGrid();
+    drawGrid(gridSize);
 }
