1d25d9a92b2214c606d9fc52ff947aea73b2da97
seldin15 <seldin_r@hotmail.com>
2021-08-26 21:47:10 +0200

    Added random color functionality

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 
 const button = document.getElementById('play');
+const randomBtn = document.getElementById('rgb');
 const containerGrid = document.querySelector('.containerGrid');
 
-
+randomBtn.addEventListener("click", function() {
+    let grid = document.getElementsByClassName('columns');
+    for(let i = 0; i < grid.length; i++) {
+        grid[i].addEventListener("mouseenter", function(event) {
+            event.target.style.backgroundColor = getRandomColor();
+        });
+    }
+});
 button.addEventListener("click", () => {
     resetGrid();
 });
@@ -12,8 +20,10 @@ function drawGrid(gridSize) {
         const rows = document.createElement('div');
         rows.classList.add('rows');
         containerGrid.appendChild(rows);
+
         let rowHeight = containerGrid.offsetHeight;
         rowHeight = rowHeight / gridSize;
+
         let allRows = document.querySelectorAll('.rows');
         allRows.forEach(rows => {
             rows.style.height = rowHeight + "px";
@@ -22,14 +32,16 @@ function drawGrid(gridSize) {
             const columns = document.createElement('div');
             columns.classList.add('columns');
             rows.appendChild(columns);
+
             columns.addEventListener("mouseenter", function(event) {
-                event.target.style.backgroundColor = "yellow";
+                event.target.style.backgroundColor = "#7A28CB";
             });
 
             let colHeight = containerGrid.offsetHeight;
             let colWidth = containerGrid.offsetWidth;
             colHeight = colHeight / gridSize;
             colWidth = colWidth / gridSize;
+
             let allColHW = document.querySelectorAll('.columns');
             allColHW.forEach(cols => {
                 cols.style.height = colHeight + "px";
@@ -37,6 +49,15 @@ function drawGrid(gridSize) {
             })
         }
 }
+}
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF'.split('');
+    let color = "#";
+    for(let i = 0; i < 6; i++) {
+        color += letters[Math.round(Math.random()* 15)];
+    }
+    return color;
 }
 
 function removeGrid() {
@@ -48,6 +69,11 @@ function removeGrid() {
 
 function resetGrid() {
     let gridSize = prompt("Type in grid size (Max 100)", "");
+    if(gridSize > 100) {
+        alert("Insert a number below 100!");
+    }
+    else {
     removeGrid();
     drawGrid(gridSize);
+}
 }
